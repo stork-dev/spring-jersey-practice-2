167775354a5b68097d64cde3362e6d386a36558c
stork-dev <email@email.com>
2022-11-02 20:44:26 +0530

    add Jetty Server and remove tomcat

--- src/main/java/com/dev/spring/DemoApplication.java
+++ src/main/java/com/dev/spring/DemoApplication.java
@@ -3,6 +3,8 @@ package com.dev.spring;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
